092c7d0325759e9c0d1d8c5130a34d94823a9ec6
infancy_m <326948850@qq.com>
2020-01-15 00:15:02 +0800

    uv texture

--- src/textures/checkerboard.h
+++ src/textures/checkerboard.h
@@ -61,15 +61,21 @@ class Checkerboard2DTexture : public Texture<T> {
           tex1(tex1),
           tex2(tex2),
           aaMethod(aaMethod) {}
-    T Evaluate(const SurfaceInteraction &si) const {
+
+    T Evaluate(const SurfaceInteraction &si) const 
+    {
         Vector2f dstdx, dstdy;
         Point2f st = mapping->Map(si, &dstdx, &dstdy);
-        if (aaMethod == AAMethod::None) {
+
+        if (aaMethod == AAMethod::None) 
+        {
             // Point sample _Checkerboard2DTexture_
             if (((int)std::floor(st[0]) + (int)std::floor(st[1])) % 2 == 0)
                 return tex1->Evaluate(si);
             return tex2->Evaluate(si);
-        } else {
+        } 
+        else 
+        {
             // Compute closed-form box-filtered _Checkerboard2DTexture_ value
 
             // Evaluate single check if filter is entirely inside one of them
@@ -77,8 +83,9 @@ class Checkerboard2DTexture : public Texture<T> {
             Float dt = std::max(std::abs(dstdx[1]), std::abs(dstdy[1]));
             Float s0 = st[0] - ds, s1 = st[0] + ds;
             Float t0 = st[1] - dt, t1 = st[1] + dt;
-            if (std::floor(s0) == std::floor(s1) &&
-                std::floor(t0) == std::floor(t1)) {
+
+            if (std::floor(s0) == std::floor(s1) && std::floor(t0) == std::floor(t1)) 
+            {
                 // Point sample _Checkerboard2DTexture_
                 if (((int)std::floor(st[0]) + (int)std::floor(st[1])) % 2 == 0)
                     return tex1->Evaluate(si);
@@ -86,11 +93,13 @@ class Checkerboard2DTexture : public Texture<T> {
             }
 
             // Apply box filter to checkerboard region
-            auto bumpInt = [](Float x) {
+            auto bumpInt = [](Float x) 
+            {
                 return (int)std::floor(x / 2) +
                        2 * std::max(x / 2 - (int)std::floor(x / 2) - (Float)0.5,
                                     (Float)0);
             };
+
             Float sint = (bumpInt(s1) - bumpInt(s0)) / (2 * ds);
             Float tint = (bumpInt(t1) - bumpInt(t0)) / (2 * dt);
             Float area2 = sint + tint - 2 * sint * tint;
@@ -115,13 +124,12 @@ class Checkerboard3DTexture : public Texture<T> {
                           const std::shared_ptr<Texture<T>> &tex1,
                           const std::shared_ptr<Texture<T>> &tex2)
         : mapping(std::move(mapping)), tex1(tex1), tex2(tex2) {}
-    T Evaluate(const SurfaceInteraction &si) const {
+
+    T Evaluate(const SurfaceInteraction &si) const 
+    {
         Vector3f dpdx, dpdy;
         Point3f p = mapping->Map(si, &dpdx, &dpdy);
-        if (((int)std::floor(p.x) + (int)std::floor(p.y) +
-             (int)std::floor(p.z)) %
-                2 ==
-            0)
+        if ( ((int)std::floor(p.x) + (int)std::floor(p.y) + (int)std::floor(p.z)) % 2 == 0 )
             return tex1->Evaluate(si);
         else
             return tex2->Evaluate(si);
